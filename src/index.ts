fc5467787d5ce3a471e1d4b846c20c8acae7d1e3
Nonpawit Pothinil <mosaggpothinil@gmail.com>
2023-09-28 15:33:19 +0700

    Update Class

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,56 @@
 import axios from 'axios';
 
+type AccessToken = {
+	app_id: string;
+	client_token: string;
+};
+/**
+ *  FacebookDevice เป็น libray ที่ใช้สำหรับการทำล็อกอินแบบ Token ใน Facebook
+ */
 class FacebookDevice {
-	private ACCESS_TOKEN: string;
-	constructor(ACCESS_TOKEN: string) {
-		this.ACCESS_TOKEN = ACCESS_TOKEN;
+	private accessToken: AccessToken;
+	private app_id: string;
+	private client_token: string;
+	private ACCESSTOKEN: string;
+	private ENDPOINT = 'https://graph.facebook.com/v18.0';
+
+	/**
+	 *
+	 * @param {string} ACCESS_TOKEN {}
+	 */
+	constructor(accessToken: AccessToken) {
+		this.accessToken = accessToken;
+		this.app_id = this.accessToken.app_id;
+		this.client_token = this.accessToken.client_token;
+		this.ACCESSTOKEN = `${this.app_id}|${this.client_token}`;
 	}
 
-	public async getCode(scope: string[]) {
+	/**
+	 *
+	 * @param {string} scope
+	 * @returns
+	 */
+	public async getDeviceCode(scope: string[]) {
 		const scopeString = scope.join(',');
-		const response = await axios(
-			'https://graph.facebook.com/v18.0/device/login',
-			{
-				method: 'POST',
-				data: {
-					access_token: this.ACCESS_TOKEN,
-					scope: scope,
-				},
-			}
-		);
+		const response = await axios(`${this.ENDPOINT}/device/login`, {
+			method: 'POST',
+			data: {
+				access_token: this.ACCESSTOKEN,
+				scope: scopeString,
+			},
+		});
 
 		return response.data;
 	}
 
-	public async getUesrToken(code: string) {
-		const response = await axios(
-			'https://graph.facebook.com/v18.0/device/login_status',
-			{
-				method: 'POST',
-				data: {
-					access_token: this.ACCESS_TOKEN,
-					code: code,
-				},
-			}
-		);
+	public async getUesrToken(codeRef: string) {
+		const response = await axios(`${this.ENDPOINT}/device/login_status`, {
+			method: 'POST',
+			data: {
+				access_token: this.ACCESSTOKEN,
+				code: codeRef,
+			},
+		});
 		return response.data;
 	}
 
@@ -40,7 +58,7 @@ class FacebookDevice {
 		let filedString = fields.join(',');
 
 		const response = await axios(
-			`https://graph.facebook.com/v2.3/me?fields=${filedString}&access_token=${userToken}`,
+			`${this.ENDPOINT}/me?fields=${filedString}&access_token=${userToken}`,
 			{
 				method: 'GET',
 			}
